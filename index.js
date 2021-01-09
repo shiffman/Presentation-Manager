5b5b4b9f320b5d9c55da57fe0491e3697ba44778
Omar-ALkhateeb <omar.alkhateeb9919@gmail.com>
2021-01-10 00:28:57 +0300

    moved the clock to the server side and fixed saving issues

--- index.js
+++ index.js
@@ -1,35 +1,99 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
-app.use(express.static('public'));
+const server = app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
+app.use(express.static("public"));
 app.use(express.json());
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 let session;
 
-app.get('/session', (request, response) => {
-  session = JSON.parse(fs.readFileSync('schedule-sample.json', 'utf-8'));
+// stop flag to halt student counter
+let stopFlag = false;
+
+// someonre is presenting
+let startedFlag = false;
+
+app.get("/session", (request, response) => {
+  session = JSON.parse(fs.readFileSync("schedule-sample.json", "utf-8"));
   response.json(session);
 });
 
-io.on('connection', (socket) => {
-  console.log('New client: ' + socket.id);
-  socket.on('new presenter', (data) => {
-    console.log(data);
-    io.emit('start presenter', data);
+io.on("connection", (socket) => {
+  console.log("New client: " + socket.id);
+  socket.on("new presenter", (data) => {
+    if (startedFlag) {
+      console.log("already presenting stop this one first!");
+    } else {
+      console.log(data);
+      startPresenter(data);
+    }
   });
-  socket.on('disconnect', () => {
-    console.log('Client disconnected ' + socket.id);
+  socket.on("stop presenter", (data) => {
+    stopFlag = true;
+    console.log("stopped");
+    io.emit("stop presenter", "done");
   });
-  socket.on('storeSession', data => {
-    console.log('Saving', data);
-    fs.writeFileSync('schedule-sample.json', JSON.stringify(data), {
-      flag: 'w',
-      encoding: 'utf8'
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected " + socket.id);
+  });
+  socket.on("storeSession", (data) => {
+    storeSession(data);
   });
 });
+
+function storeSession(data) {
+  // console.log("Saving", data);
+  fs.writeFileSync("schedule-sample.json", JSON.stringify(data), {
+    flag: "w",
+    encoding: "utf8",
+  });
+}
+
+async function startPresenter(data) {
+  session = JSON.parse(fs.readFileSync("schedule-sample.json", "utf-8"));
+
+  startedFlag = true;
+  let index = 0;
+  for (let i in session["presenters"]) {
+    if (session["presenters"][i].name == data["name"]) index = i;
+  }
+  let presenter = session["presenters"][index];
+  presenter["minutes"] = parseInt(data["minutes"]);
+  presenter["seconds"] = parseInt(data["seconds"]);
+
+  while (presenter["minutes"] !== 0 || presenter["seconds"] !== 0) {
+    // console.log("running");
+    // break if a stop signal is sent
+    if (stopFlag) {
+      break;
+    }
+    // sleep for 1s
+    await new Promise((r) => setTimeout(r, 1000));
+
+    if (presenter["seconds"] == 0) {
+      presenter["minutes"]--;
+      presenter["seconds"] = 60;
+    }
+
+    presenter["seconds"]--;
+
+    session["presenters"][index] = presenter;
+    io.emit("user", presenter);
+    io.emit("admin", session);
+    storeSession(session);
+  }
+
+  session["presenters"][data["name"]] = presenter;
+  io.emit("user", presenter);
+  io.emit("admin", session);
+  storeSession(session);
+
+  startedFlag = false;
+  stopFlag = false;
+}
--- public/admin/assets/js/sketch.js
+++ public/admin/assets/js/sketch.js
@@ -11,45 +11,78 @@ let presenterDiv;
 const socket = io();
 
 async function loadSession() {
-  const response = await fetch('/session');
+  const response = await fetch("/session");
   session = await response.json();
   // extra spot for when we are done
   drawSession();
 }
 
+socket.on("admin", (data) => {
+  session = data;
+  console.log(data);
+  drawSession();
+});
 
 function drawSession() {
   removeElements(); // clear old session data
   const inputs = [];
   const TinMin = [];
+  const TinSec = [];
 
   // extra spot for when we are done
-  session.presenters.push('💖');
+  // session.presenters.push({ name: "💖", minutes: 1 });
 
   for (let i = 0; i < session.presenters.length; i++) {
-    const name = session.presenters[i];
-    const T = session.minutes;
+    const name = session.presenters[i].name;
+    const T = session.presenters[i].minutes;
+    const S = session.presenters[i].seconds;
 
-    const presenterDiv = createDiv('');
+    const presenterDiv = createDiv("");
     createSpan(`${i + 1}: `).parent(presenterDiv);
-    inputs[i] = createInput(name).parent(presenterDiv).addClass(`${i}-name name`);
-    TinMin[i] = createInput(str(T)).parent(presenterDiv).addClass(`${i}-timer timer`);
-    const start = createButton('start')
+    inputs[i] = createInput(name)
+      .parent(presenterDiv)
+      .addClass(`${i}-name name`);
+    TinMin[i] = createInput(str(T))
+      .parent(presenterDiv)
+      .addClass(`${i}-timer timer`);
+    TinSec[i] = createInput(str(S))
+      .parent(presenterDiv)
+      .addClass(`${i}-timer timer`);
+    const start = createButton("start")
       .parent(presenterDiv)
       .mousePressed(() => {
-        socket.emit('new presenter', {
+        socket.emit("new presenter", {
           name: inputs[i].value(),
-          next: (inputs[i + 1] != null) ? inputs[i + 1].value() : 'All done!', // perhaps change this to a more desirable behavior
+          next: inputs[i + 1] != null ? inputs[i + 1].value() : "All done!", // perhaps change this to a more desirable behavior
           minutes: TinMin[i].value(),
+          seconds: TinSec[i].value(),
         });
       });
-    // const remove = createButton('remove')
-    //   .parent(presenterDiv)
-    //   .mousePressed(() => {
-    //     session.presenters.splice(i, 1);
-    //     drawSession();
-    //   });
+
+    const pop = createButton("delete")
+      .parent(presenterDiv)
+      .mousePressed(() => {
+        session.presenters.splice(i, 1);
+        socket.emit("storeSession", session);
+        drawSession();
+      });
   }
+
+  const presenterDiv = createDiv("");
+  const push = createButton("push")
+    .parent(presenterDiv)
+    .mousePressed(() => {
+      session.presenters.push({ name: "", minutes: 1, seconds: 0 });
+      socket.emit("storeSession", session);
+      drawSession();
+    });
+
+  const presenterDiv2 = createDiv("");
+  const stop = createButton("stop")
+    .parent(presenterDiv2)
+    .mousePressed(() => {
+      socket.emit("stop presenter", {});
+    });
 
   // curently broken
   // createElement('br').parent(presenterDiv);
@@ -59,10 +92,16 @@ function drawSession() {
   //     session.presenters.push('');
   //     drawSession();
   //   });
-  // // TODO: Save minutes? (need to modify core JSON structure)
-  // const save = createButton('Save JSON')
-  //   .parent(presenterDiv)
-  //   .mousePressed(() => {
-  //     socket.emit('storeSession', session);
-  //   });
+
+  const save = createButton("Save JSON")
+    .parent(presenterDiv)
+    .mousePressed(() => {
+      for (let i in session.presenters) {
+        session.presenters[i].name = inputs[i].value();
+        session.presenters[i].minutes = TinMin[i].value();
+        session.presenters[i].seconds = TinSec[i].value();
+        // console.log(inputs[i]);
+      }
+      socket.emit("storeSession", session);
+    });
 }
